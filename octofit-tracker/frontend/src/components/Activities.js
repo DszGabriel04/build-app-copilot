f82c25eaaeb98cd9be045013408f0efe124199d2
Gabriel Allwyn D Souza <114800260+DszGabriel04@users.noreply.github.com>
2025-06-03 11:07:22 +0000

    q2

--- octofit-tracker/frontend/src/components/Activities.js
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -4,7 +4,7 @@ function Activities() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    fetch('https://solid-fiesta-v99jw4wp97pfw6xv-8000.app.github.dev/api/activity/?format=api')
+    fetch('https://solid-fiesta-v99jw4wp97pfw6xv-8000.app.github.dev/api/activity/')
       .then(response => response.json())
       .then(data => setActivities(data));
   }, []);
